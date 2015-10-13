ae6a27b87245b8339afa458ec3adb62fd2705487
Dominik Weickgenannt <dowei14@student.sdu.dk>
2015-10-13 18:38:30 +0200

    updated TF broadcaster

--- MR_TFBroadcaster/src/tfbroadcaster.cpp
+++ MR_TFBroadcaster/src/tfbroadcaster.cpp
@@ -1,45 +1,23 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
-#include <nav_msgs/Odometry.h>
-
-std::string turtle_name;
-
-void poseCallback(const nav_msgs::Odometry::ConstPtr& msg){
-  static tf::TransformBroadcaster br;
-/*  tf::Transform transform;
-  transform.setOrigin( tf::Vector3(1.0, 2.0, 0.0) );
-  tf::Quaternion q;
-  q.setRPY(0, 0, 3.0);
-  transform.setRotation(q);
-
-  br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", turtle_name));
-*/
-
-
-  geometry_msgs::TransformStamped lidar_trans;
-  geometry_msgs::Quaternion odom_quat = msg->pose.pose.orientation;
-  //const geometry_msgs::Quaternion quat = msg->pose.pose.orientation;
-  double tmp = tf::getYaw(odom_quat);
-  odom_quat = tf::createQuaternionMsgFromRollPitchYaw(M_PI,0.0,0.0);
-
-  lidar_trans.header.stamp = ros::Time::now();
-  lidar_trans.header.frame_id = "lidar_frame";
-  lidar_trans.child_frame_id = "odom_combined";
-  lidar_trans.transform.translation.x = -0.13;//msg->pose.pose.position.x;
-  lidar_trans.transform.translation.y = 0.0;//msg->pose.pose.position.y;
-  lidar_trans.transform.translation.z = 0.14;
-  lidar_trans.transform.rotation = odom_quat;
-
-  br.sendTransform(lidar_trans);
-
-}
 
 int main(int argc, char** argv){
-  ros::init(argc, argv, "lidar_tf_broadcaster");
-  turtle_name = "test";
+  ros::init(argc, argv, "lidar_tf");
   ros::NodeHandle node;
-  ros::Subscriber sub = node.subscribe("/fmKnowledge/pose", 10, &poseCallback);
 
-  ros::spin();
+  std::string lidarFrame, vehicleFrame;
+  node.param<std::string>("lidar_frame", lidarFrame, "lidar_frame");
+  node.param<std::string>("vehicle_frame", vehicleFrame, "base_footprint");
+
+  tf::TransformBroadcaster br;
+  tf::Transform transform;
+
+  ros::Rate rate(10.0);
+  while (node.ok()){
+    transform.setOrigin( tf::Vector3(0.01, 0.0, 0.14) );
+    transform.setRotation( tf::createQuaternionFromRPY(M_PI,0,0) );
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), vehicleFrame, lidarFrame));
+    rate.sleep();
+  }
   return 0;
 };

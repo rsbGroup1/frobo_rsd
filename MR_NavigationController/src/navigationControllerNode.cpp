4633a21f6472bae7e7f19c81173bd942f5271146
veimox <veimox@gmail.com>
2015-11-04 09:22:44 +0100

    Changed Node structure to Class

--- MR_NavigationController/src/navigationControllerNode.cpp
+++ MR_NavigationController/src/navigationControllerNode.cpp
@@ -1,176 +1,180 @@
 // Includes
-#include <ros/ros.h>
 #include <cstdlib>
 #include <string>
 #include <iostream>
 #include <sstream>
+
+#include <ros/ros.h>
 #include "mr_navigation_controller/performAction.h"
 #include "mr_navigation_controller/enable.h"
 #include "mr_navigation_controller/move.h"
-#include <boost/thread.hpp>
-#include <boost/thread/mutex.hpp>
 #include "std_msgs/String.h"
 #include "std_msgs/Float64.h"
 #include "mr_line_follower/followUntilQR.h"
 
-// Defines
-#define M_PI                    3.14159265358979323846
-#define DEGREETORAD             (M_PI/180.0)
-#define RADTODEGREE             (180.0/M_PI)
-#define SSTR(x)                 dynamic_cast< std::ostringstream & >(( std::ostringstream() << std::dec << x )).str()
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
 
-// Skill struct
+// Defines
+#define M_PI		3.14159265358979323846
+#define DEG_TO_RAD	(M_PI/180.0)
+#define RAD_TO_DEG	(180.0/M_PI)
+#define SSTR(x)		dynamic_cast< std::ostringstream & >(( std::ostringstream() << std::dec << x )).str()
+
+/**
+ * Skill struct
+ */
 struct Skill
 {
     //
 };
 
-// Global var
-ros::Publisher _statusTopic;
-ros::ServiceClient _serviceLineFollow, _serviceMove;
 
-// Skills
-bool lineUntilQR(int QR)
+class NavigationController{
+public:
+	/**
+	 * Constructors
+	 */
+	NavigationController() :
+		      pNh_("~")
+	{	
+		// Get parameter names
+		
+		pNh_.param<std::string>("lineFollowEnableService", srv_lineUntilQR_name_, "mrLineFollower/enable");
+		pNh_.param<std::string>("moveService", srv_move_name_, "mrGo/move");
+		pNh_.param<std::string>("performAction", srv_action_name_, "mrNavigationController/performAction");
+		pNh_.param<std::string>("status", pub_status_name_, "mrNavigationController/status");
+		
+		// Service
+		srv_lineUntilQR_ = nh_.serviceClient<mr_navigation_controller::enable>(srv_lineUntilQR_name_);
+		srv_move_ = nh_.serviceClient<mr_navigation_controller::move>(srv_move_name_);
+		srv_action_ = nh_.advertiseService (
+			srv_action_name_, &NavigationController::performActionCallback, this);
+		
+		// Publisher
+		pub_status_ = nh_.advertise<std_msgs::String>(pub_status_name_, 10);
+	}
+	
+	~NavigationController(){
+		//
+	}
+	
+	
+	/**
+	 * Skills
+	 */
+	bool lineUntilQR(std::string qr)
+	{
+		mr_line_follower::followUntilQR lineFollowerCall;
+		lineFollowerCall.request.qr = qr;
+		lineFollowerCall.request.time_limit = 30;
+		return true;
+	}
+	
+	bool linearMove(double distance)
+	{
+		mr_navigation_controller::move obj;
+		obj.request.linear = distance;
+		      srv_move_.call(obj);
+		return obj.response.done;
+	}
+	
+	bool angularMove(double distance)
+	{
+		mr_navigation_controller::move obj;
+		obj.request.angle = distance;
+		      srv_move_.call(obj);
+		return obj.response.done;
+	}
+	
+	bool goToFreePosition(double x, double y)
+	{
+		
+	}
+	
+	bool moveToDispenser()
+	{
+		
+	}
+	
+	bool moveToCharger()
+	{
+		
+	}
+	
+	bool moveFromDispenser()
+	{
+		
+	}
+	
+	bool moveFromCharger()
+	{
+		
+	}
+	
+	bool changeLineWC1()
+	{
+		
+	}
+	
+	bool changeLineWC2()
+	{
+		
+	}
+	
+	bool changeLineWC3()
+	{
+		
+	}
+	
+	std::vector<Skill> graphSearch(int action)
+	{
+		return std::vector<Skill>();
+	}
+	
+	bool performActionCallback(mr_navigation_controller::performAction::Request &req,
+							   mr_navigation_controller::performAction::Response &res)
+	{
+		// Search in graph how to perform action
+		std::vector<Skill> skillVec = graphSearch(req.action);
+		
+		// Execute skills
+		for(unsigned int i = 0; i<skillVec.size(); i++)
+		{
+			//
+			std_msgs::String msg;
+			msg.data = "Blabla";
+			         pub_status_.publish(msg);
+		}
+		
+		// Return status
+		return true;
+	}
+	
+private:
+	ros::NodeHandle nh_, pNh_;
+	ros::Publisher pub_status_;
+	ros::ServiceClient srv_lineUntilQR_, srv_move_;
+	ros::ServiceServer srv_action_;
+	std::string srv_lineUntilQR_name_, srv_move_name_, pub_status_name_, srv_action_name_;
+};
+
+
+/**
+ * Main
+ */
+int main(int argc, char** argv)
 {
-    mr_navigation_controller::enable obj;
-    obj.request.enable = true;
-    if(!_serviceLineFollow.call(obj))
-    {
-        ROS_ERROR("Could not start line following!");
-        return false;
-    }
-
-    while(true)
-    {
-        // WAIT TILL QR IS FOUND
-    }
-
-    obj.request.enable = false;
-    if(!_serviceLineFollow.call(obj))
-    {
-        ROS_ERROR("Could not stop line following!");
-        return false;
-    }
-
-    return true;
-}
-
-bool linearMove(double distance)
-{
-    mr_navigation_controller::move obj;
-    obj.request.linear = distance;
-    _serviceMove.call(obj);
-    return obj.response.done;
-}
-
-bool angularMove(double distance)
-{
-    mr_navigation_controller::move obj;
-    obj.request.angle = distance;
-    _serviceMove.call(obj);
-    return obj.response.done;
-}
-
-bool goToFreePosition(double x, double y)
-{
-
-}
-
-bool moveToDispenser()
-{
-
-}
-
-bool moveToCharger()
-{
-
-}
-
-bool moveFromDispenser()
-{
-
-}
-
-bool moveFromCharger()
-{
-
-}
-
-bool changeLineWC1()
-{
-
-}
-
-bool changeLineWC2()
-{
-
-}
-
-bool changeLineWC3()
-{
-
-}
-
-std::vector<Skill> graphSearch(int action)
-{
-    return std::vector<Skill>();
-}
-
-bool performActionCallback(mr_navigation_controller::performAction::Request &req, mr_navigation_controller::performAction::Response &res)
-{
-    // Search in graph how to perform action
-    std::vector<Skill> skillVec = graphSearch(req.action);
-
-    // Execute skills
-    for(unsigned int i = 0; i<skillVec.size(); i++)
-    {
-        //
-        std_msgs::String msg;
-        msg.data = "Blabla";
-        _statusTopic.publish(msg);
-    }
-
-    // Return status
-    return true;
-}
-
-void qrCallback(std_msgs::String msg)
-{
-
-}
-
-// Functions
-int main()
-{
-    // Setup ROS Arguments
-    char** argv = NULL;
-    int argc = 0;
-
-    // Init ROS Node
+	// Init ROS Node
     ros::init(argc, argv, "MR_Navigation_Controller");
-    ros::NodeHandle nh, pNh("~");
-
-    // Get parameter names
-    std::string lineFollowEnableString, moveString, qrSub;
-    pNh.param<std::string>("lineFollowEnableService", lineFollowEnableString, "mrLineFollower/enable");
-    pNh.param<std::string>("moveService", moveString, "mrGo/move");
-    pNh.param<std::string>("qrSub", qrSub, "/mrCameraProcessing/QR");
-
-    // Service
-    _serviceLineFollow = nh.serviceClient<mr_navigation_controller::enable>(lineFollowEnableString);
-    _serviceMove = nh.serviceClient<mr_navigation_controller::move>(moveString);
-    ros::ServiceServer actionServer = nh.advertiseService("mrNavigationController/performAction", performActionCallback);
-
-    // Subscriber
-    ros::Subscriber qrSubscriber = nh.subscribe(qrSub, 10, qrCallback);
-
-    // Publisher
-    _statusTopic = nh.advertise<std_msgs::String>("mrNavigationController/status", 10);
-
-    // ROS Spin: Handle callbacks
-    while(ros::ok())
+	NavigationController nc;
+	ros::Rate rate(30);
+    
+	// ROS Spin: Handle callbacks
+    while(ros::isShuttingDown()){
         ros::spinOnce();
-
+		rate.sleep();
+	}
     // Return
     return 0;
 }

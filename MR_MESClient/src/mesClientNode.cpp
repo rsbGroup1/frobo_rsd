f3ec1a5fe9b58b86bb74f41581498f8ea0328a63
Yonas Alizadeh <yoali11@student.sdu.dk>
2015-11-18 15:07:31 +0100

    Changed message type

--- MR_MESClient/src/mesClientNode.cpp
+++ MR_MESClient/src/mesClientNode.cpp
@@ -21,11 +21,20 @@ ros::Publisher _mesMessagePub;
 std::string _serverIP;
 int _serverPort;
 int _socket;
+bool _connected = false;
+bool _waitForServer = true;
+bool _waitForStatus = false;
 
 // Functions
 void sendMsgCallback(std_msgs::String msg)
 {
-    // Construct and send message to server
+    if(_connected)
+    {
+        // Send data
+        write(_socket, msg.data.c_str(), msg.data.size());
+        _waitForServer = true;
+        _waitForStatus = true;
+    }
 }
 
 bool connectToServer()
@@ -44,11 +53,17 @@ bool connectToServer()
     connect(_socket, (sockaddr*)&addr, sizeof(addr));
 
     // Test connection
-    int writeSize = write(_socket, "Cell 1", 7);
+    int writeSize = write(_socket, "MR1", 4);
     if(writeSize < 0)
+    {
+        _connected = false;
         return false;
+    }
     else
+    {
+        _connected = true;
         return true;
+    }
 }
 
 int main()
@@ -66,7 +81,7 @@ int main()
     std::string mesSub, mesPub;
     pNh.param<std::string>("mesPub", mesPub, "/mrMESClient/msgFromServer");
     pNh.param<std::string>("mesSub", mesSub, "/mrMESClient/msgToServer");
-    pNh.param<std::string>("server_ip", _serverIP, "10.115.253.233");
+    pNh.param<std::string>("server_ip", _serverIP, "127.0.0.1");//10.115.253.233");
     pNh.param<int>("server_port", _serverPort, 21240);
 
     // Publishers
@@ -88,46 +103,70 @@ int main()
     // Set loop rate
     while(ros::ok())
     {
-        char buffer[BUFFER_SIZE];
-        int readSize = read(_socket, buffer, BUFFER_SIZE);
-
-        if(readSize <= 0)
+        if(_waitForServer)
         {
-            ROS_ERROR("No message from MES Server!");
-        }
-        else
-        {
-            std::string msg(buffer);
-            msg = msg.substr(0, msg.size()-1);
-            std::cout << msg << std::endl;
+            char buffer[BUFFER_SIZE];
+            int readSize = read(_socket, buffer, BUFFER_SIZE);
 
-            // Open document
-            tinyxml2::XMLDocument doc;
-            if(doc.Parse(msg.c_str()) != 0)
+            if(readSize <= 0)
             {
-                ROS_ERROR("Error parsing string!");
-                return false;
+                ROS_ERROR("No message from MES Server!");
+                break;
             }
-
-            int cell, mobileRobot, red, blue, yellow;
-
-            // Check if "MESServer"
-            if(std::string(doc.RootElement()->Value()) == "MESServer")
+            else
             {
-                // Get stuff
-                cell = atoi(doc.RootElement()->FirstChildElement("Cell")->FirstChild()->Value());
-                mobileRobot = atoi(doc.RootElement()->FirstChildElement("MobileRobot")->FirstChild()->Value());
-                red = atoi(doc.RootElement()->FirstChildElement("Red")->FirstChild()->Value());
-                blue = atoi(doc.RootElement()->FirstChildElement("Blue")->FirstChild()->Value());
-                yellow = atoi(doc.RootElement()->FirstChildElement("Yellow")->FirstChild()->Value());
+                std::string MESServer = "</MESServer>";
+                std::string msg(buffer);
+                size_t found = msg.find(MESServer);
+                msg = msg.substr(0, found+12);
+                std::cout << msg << std::endl;
 
-                mr_mes_client::server msg;
-                msg.blue = blue;
-                msg.cell = cell;
-                msg.mobileRobot = mobileRobot;
-                msg.yellow = yellow;
-                msg.red = red;
-                _mesMessagePub.publish(msg);
+                // Open document
+                tinyxml2::XMLDocument doc;
+                if(doc.Parse(msg.c_str()) != 0)
+                {
+                    ROS_ERROR("Error parsing string!");
+                    break;
+                }
+
+                int cell, mobileRobot, red, blue, yellow, status;
+
+                // Check if "MESServer"
+                if(std::string(doc.RootElement()->Value()) == "MESServer")
+                {
+                    mr_mes_client::server msg;
+
+                    if(_waitForStatus)
+                    {
+                        // Get stuff
+                        status = atoi(doc.RootElement()->FirstChildElement("Status")->FirstChild()->Value());
+
+                        msg.mobileRobot = 1;
+                        msg.status = status;
+
+                        // Reset
+                        _waitForStatus = false;
+                    }
+                    else
+                    {
+                        // Get stuff
+                        cell = atoi(doc.RootElement()->FirstChildElement("Cell")->FirstChild()->Value());
+                        mobileRobot = atoi(doc.RootElement()->FirstChildElement("MobileRobot")->FirstChild()->Value());
+                        red = atoi(doc.RootElement()->FirstChildElement("Red")->FirstChild()->Value());
+                        blue = atoi(doc.RootElement()->FirstChildElement("Blue")->FirstChild()->Value());
+                        yellow = atoi(doc.RootElement()->FirstChildElement("Yellow")->FirstChild()->Value());
+
+                        msg.blue = blue;
+                        msg.cell = cell;
+                        msg.mobileRobot = mobileRobot;
+                        msg.yellow = yellow;
+                        msg.red = red;
+                    }
+
+                    _mesMessagePub.publish(msg);
+                }
+
+                _waitForServer = false;
             }
         }
 
